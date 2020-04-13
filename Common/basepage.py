bc9ebc8186414a08a02f967f0e5d39d1f9151428
ounula <379923145@qq.com>
2020-04-13 15:09:33 +0800

    Initial commit

--- Common/basepage.py
+++ Common/basepage.py
@@ -6,7 +6,7 @@ from Common.filepath import FilePath
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
 import time,datetime
-import os
+
 #封装基本函数 - 执行日志、异常处理、失败截图
 #所有页面公共的部分
 class BasePage:
@@ -39,30 +39,87 @@ class BasePage:
             self.save_screenshot(doc)
             raise
 
-
     #等待元素存在
-    def wait_elePresent(self):
-        pass
+    def wait_elePresent(self,locator,times=20,poll_frequency=0.5,doc=""):
+        '''
+
+        :param locator: 元素定位，元祖形式
+        :param times: 最长等待时间
+        :param poll_frequency: 轮询间隔
+        :param doc: 模块名_页面名称_操作名称
+        :return:
+        '''
+        logging.info("判断网页存在元素{0}".format(locator))
+        try:
+            # 开始等待时间
+            start = time.time()
+            WebDriverWait(self.driver, times, poll_frequency).until(EC.presence_of_element_located((locator)))
+            # 结束等待的时间点
+            end = time.time()
+            # 求差值
+            wait_time = '%.1f' % (end - start)
+            logging.info("网页存在元素，花费{0}秒".format(wait_time))
+        except:
+            logging.exception("定位元素超时")
+            # 截图
+            self.save_screenshot(doc)
+            raise
 
     #查找元素
-    def get_element(self):
-        pass
+    def get_element(self,locator,doc=""):
+        logging.info("开始查找元素:{}".format(locator))
+        try:
+            self.driver.find_element(*locator)
+        except:
+            logging.exception("查找元素失败！")
+            #截图
+            self.save_screenshot(doc)
+            raise
 
     #点击操作
-    def click_element(self):
-        pass
+    def click_element(self,locator,doc=""):
+        ele = self.get_element(locator,doc=doc)
+        logging.info("{0}   点击元素：{1}".format(doc,locator))
+        try:
+            ele.click()
+        except:
+            logging.exception("元素点击操作失败！")
+            #  截图
+            self.save_screenshot(doc)
+            raise
 
     #输入操作
-    def input_text(self):
-        pass
+    def input_text(self,locator,text,doc=""):
+        ele = self.get_element(locator,doc=doc)
+        try:
+            ele.send_keys(text)
+        except:
+            logging.exception("输入操作失败！")
+            #  截图
+            self.save_screenshot(doc)
+            raise
 
     #获取元素的文本内容
-    def get_text(self):
-        pass
+    def get_text(self,locator,doc=""):
+        ele = self.get_element(locator, doc=doc)
+        try:
+            return ele.text
+        except:
+            logging.exception("获取元素文本失败！")
+            #  截图
+            self.save_screenshot(doc)
+            raise
 
     #获取元素属性
-    def get_element_attribute(self):
-        pass
+    def get_element_attribute(self,locator,attr,doc=""):
+        ele = self.get_element(locator, doc=doc)
+        try:
+            return ele.get_attribute(attr)
+        except:
+            logging.exception("输入操作失败！")
+            #  截图
+            self.save_screenshot(doc)
+            raise
 
     #alert处理
     def alert_action(self,action="accept"):
--- Common/debug.py
+++ Common/debug.py
@@ -1,7 +1,12 @@
 # -*- encoding:utf-8 -*-
 # @Time : 2020/4/13 12:44 
 # @Author : ZHH
-import datetime,time
-import os
-c=os.path.join(os.path.abspath(os.path.dirname(os.getcwd())),"Outputs","screenshots")
-print(c)
+from selenium import webdriver
+from Common.filepath import FilePath
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
+import time,datetime
+from selenium.webdriver.common.by import By
+driver=webdriver.Chrome()
+a=driver.find_element_by_xpath('123')
+a.get_attribute()
