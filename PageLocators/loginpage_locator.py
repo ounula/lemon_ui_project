a32e6d7978117f2340fba4387261b0ea956a3403
zhh <ounule@vip.qq.com>
2020-04-15 23:36:42 +0800

    Initial commit

--- PageLocators/loginpage_locator.py
+++ PageLocators/loginpage_locator.py
@@ -7,7 +7,7 @@ class LoginPageLocator:
     pwd_text = (By.XPATH,'//input[@placeholder="请输入您的密码"]')
     login_but = (By.XPATH,'//button[@class="el-button el-button--primary"]')
     #错误提示框-密码不足6位
-    errMSG_6passwd =  (By.XPATH,'//div[contains(text(),"至少6位数的密码")]')
+    errMSG_6passwd =  (By.XPATH,'//div[contains(text(),"密码")]')
     #错误提示框-请输入账号
     errMSG_no_input_user = (By.XPATH,'//div[contains(text(),"请输入账号")]')
     #错误提示信息-账号密码错误
