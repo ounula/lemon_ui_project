438316829dfd12358047c6f0377f277063724693
zhh <ounule@vip.qq.com>
2020-05-06 05:02:24 +0800

    Initial commit

--- Common/debug.py
+++ Common/debug.py
@@ -13,7 +13,10 @@ from Common import dir_config
 import collections
 from Common.win_controller import WinControl
 from selenium import webdriver
+from Common.win_controller import WinControl
 
-
-WinControl().open_software(dir_config.MicroSip_dir,"软电话")
-
+driver = webdriver.Chrome()
+driver.get("https://www.layui.com/demo/upload.html")
+driver.implicitly_wait(10)
+driver.find_element_by_id('test2').click()
+WinControl().upload_file('D:\\','新建文本文档.txt')
--- Common/win_controller.py
+++ Common/win_controller.py
@@ -5,6 +5,8 @@ from pywinauto.application import Application
 from Common.logger import Log
 from Common.dir_config import MicroSip_dir
 from time import sleep
+from pywinauto.keyboard import send_keys
+import pywinauto
 
 class WinControl:
     # 打开指定软件
@@ -19,14 +21,28 @@ class WinControl:
         except:
             Log().log_error(doc+"开启失败")
 
-
-app = Application('uia').start(MicroSip_dir)
-sleep(2)
-app2 = Application('uia').connect(path=MicroSip_dir)
-dlg = app2['MicroSIP']
-combox_1 = dlg['ComboBox']
-edit = combox_1.child_window(auto_id="1001", control_type="Edit")
-#截图需安装pillow
-pic = dlg.capture_as_image()
-# pic.save('01.png')
-Log().log_info("开启成功")
+    def upload_file(self,file_path,file,doc='',*args):
+        try:
+            #选择文件
+            app = pywinauto.Desktop()
+            dlg = app["打开"]
+            dlg["Toolbar3"].click()
+            send_keys(file_path)
+            send_keys("{VK_RETURN}")
+            dlg["文件名(&N):Edit"].type_keys('"{}"'.format(file))
+            for i in args:
+                send_keys('"{}"'.format(i))
+            dlg["打开(&O)"].click()
+            Log().log_info(doc+"上传文件成功")
+        except:
+            Log().log_error(doc+"上传文件失败")
+# app = Application('uia').start(MicroSip_dir)
+# sleep(2)
+# app2 = Application('uia').connect(path=MicroSip_dir)
+# dlg = app2['MicroSIP']
+# combox_1 = dlg['ComboBox']
+# edit = combox_1.child_window(auto_id="1001", control_type="Edit")
+# #截图需安装pillow
+# pic = dlg.capture_as_image()
+# # pic.save('01.png')
+# Log().log_info("开启成功")
